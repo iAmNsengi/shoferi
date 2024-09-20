f01d26f0399eebea4a6c5f6c20331e7136457b9c
Eliezer Nsengi <avaranstudios@gmail.com>
2024-09-20 15:02:38 +0200

    fix: hosting configurations

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // You can change this to any port you prefer
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // This allows you to use '@' to refer to your src directory
+    },
+  },
 })
